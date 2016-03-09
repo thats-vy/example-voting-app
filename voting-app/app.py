e4043da28d6d9bd103d56deb039e8982fbe799b5
Kay Yan <kay.yan@daocloud.io>
2016-03-09 21:56:51 +0800

    update

--- voting-app/app.py
+++ voting-app/app.py
@@ -8,8 +8,8 @@ import socket
 import random
 import json
 
-option_a = os.getenv('OPTION_A', "Cats")
-option_b = os.getenv('OPTION_B', "Dogs")
+option_a = os.getenv('OPTION_A', "希拉里")
+option_b = os.getenv('OPTION_B', "川普")
 hostname = socket.gethostname()
 
 redis = connect_to_redis("redis")
